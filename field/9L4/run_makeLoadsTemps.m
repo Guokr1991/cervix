b97500738d1a1344bb37e33581b99a69e145c3e2
Mark Palmeri <mlp6@duke.edu>
2015-04-05 20:14:26 -0400

    ADD new normalization file for point load generation

--- field/9L4/run_makeLoadsTemps.m
+++ field/9L4/run_makeLoadsTemps.m
@@ -5,5 +5,5 @@ addpath('/home/mlp6/fem/trunk/field/');
 %dynaFiles = dir('dyna*.mat');
 %for i=1:length(dynaFiles),
 %    InputFile = dynaFiles(i).name;
-    makeLoadsTemps(InputFile,'dyna-I-f6.00-F2.0-FD0.025-a1.7.mat',100,100,2.0,0.01^3,'q',1);
+    makeLoadsTemps(InputFile,'dyna-I-f6.13-F1.5-FD0.030-a0.50.mat',100,100,2.0,0.01^3,'q',1);
 %end;
